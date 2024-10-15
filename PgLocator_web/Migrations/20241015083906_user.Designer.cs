b4761d3d2b6bb915d01af663d1bbf02ce7de6c29
Nandana Boban <nandanaboban03@gmail.com>
2024-10-15 15:34:11 +0530

    user

--- PgLocator_web/Migrations/20241015083906_user.Designer.cs
+++ PgLocator_web/Migrations/20241015083906_user.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PgLocator_web.Data;
 
@@ -11,9 +12,11 @@ using PgLocator_web.Data;
 namespace PgLocator_web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241015083906_user")]
+    partial class user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
